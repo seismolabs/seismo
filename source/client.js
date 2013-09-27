9d26debf3df951d9260ac341b21d2cd35efea342
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-09-27 22:10:33 +0300

    prototype of client added

--- source/client.js
+++ source/client.js
@@ -1,9 +1,26 @@
-function client(args) {
+var request = require('request');
 
-}
+module.exports = function (app) {
 
-client.query = function(args) {
+	var client = function client(eventName, callback) {
+		request.post({url: url, body: {data: eventName}, json: true}, function (err, resp) {
+			if (err) {
+				return callback({message: 'error occured during event posting', err: err});
+			}
 
+			callback(null, resp.body);
+		});
+	};
+
+	client.query = function(query) {
+		request.get({url: url, json: true}, function (err, resp) {
+			if (err) {
+				return callback({message: 'error occured during getting events', err: err});
+			}
+
+			callback(null, resp.body);
+		});
+	};
+
+	return client;
 };
-
-module.exports = client;
