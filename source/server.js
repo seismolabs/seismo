3198f9c584e6139df920499a821edb749fa1fe16
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-11-14 22:53:35 +0200

    updated cors settings

--- source/server.js
+++ source/server.js
@@ -15,7 +15,7 @@ var app = express();
 var cors = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-Token, X-Revision, Content-Type');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-Access-Token, X-Revision, Content-Type');
 
 	next();
 };
