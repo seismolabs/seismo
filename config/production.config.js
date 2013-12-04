0c14fa767ddb14b34a988f59e191b9c2c8069ad6
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-04 11:59:24 +0200

    production settings for analytics

--- config/production.config.js
+++ config/production.config.js
@@ -7,6 +7,12 @@ var config = {
 
 	logentries: {
 		token: process.env.LOGENTRIES_TOKEN
+	},
+
+	users: {
+		'alexanderbeletsky': '$2a$12$8OWQYmioFTci9/kGppVujeEmFkOVCumMRVkYKg42fopiE9nYuD4uW',
+		'voronianski': '$2a$12$5JkTzjpF8HcJNUrHrT.OiOqs80lXcaABNUKRTY51N.EJh240vsuJe',
+		'likeastore': '$2a$12$gS6K/5dt8ECKnB93BoVnse7qXTeljAH7dSlLZZrO7TPdLEYZwjnrG'
 	}
 };
 
