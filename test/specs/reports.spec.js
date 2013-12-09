c58a8d53d37d0e4f679c635632500b049a680c2c
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-09 20:08:37 +0200

    decopled from client

--- test/specs/reports.spec.js
+++ test/specs/reports.spec.js
@@ -1,9 +1,9 @@
 var moment = require('moment');
 var testUtils = require('../utils');
-var request = request('request');
+var request = require('request');
 
-describe.only('building reports', function () {
-	var app, events, url, error, options, summary, credentials, token;
+describe('building reports', function () {
+	var app, url, error, summary, credentials, token;
 
 	before(function () {
 		url = testUtils.getRootUrl();
@@ -31,100 +31,98 @@ describe.only('building reports', function () {
 		});
 	});
 
-		/*
-		describe('report by hour', function () {
-			before(function (done) {
-				events.report({event: 'application started', report: 'hour', date: '2013-09-29', hour: 6}, function (err, sum) {
-					error = err;
-					summary = sum;
-					done(err);
-				});
-			});
-
-			it('should have event data', function () {
-				expect(summary.id).to.equal('app-started');
-				expect(summary.event).to.equal('application started');
-			});
-
-			it('should have total', function () {
-				expect(summary.total).to.equal(3);
+	describe('report by hour', function () {
+		before(function (done) {
+			request.get({url: url + '/api/reports/hour/' + app + '?event=application started&date=2013-09-29&hour=6', headers: {'x-access-token': token}, json: true}, function (err, res) {
+				error = err;
+				summary = res.body;
+				done(err);
 			});
 		});
 
-		describe('report by day', function () {
-			before(function (done) {
-				events.report({event: 'application started', report: 'day', date: '2013-09-29'}, function (err, sum) {
-					error = err;
-					summary = sum;
-					done(err);
-				});
-			});
+		it('should have event data', function () {
+			expect(summary.id).to.equal('app-started');
+			expect(summary.event).to.equal('application started');
+		});
 
-			it('should have event data', function () {
-				expect(summary.id).to.equal('app-started');
-				expect(summary.event).to.equal('application started');
-			});
+		it('should have total', function () {
+			expect(summary.total).to.equal(3);
+		});
+	});
 
-			it('should have total', function () {
-				expect(summary.total).to.equal(6);
+	describe('report by day', function () {
+		before(function (done) {
+			request.get({url: url + '/api/reports/day/' + app + '?event=application started&date=2013-09-29', headers: {'x-access-token': token}, json: true}, function (err, res) {
+				error = err;
+				summary = res.body;
+				done(err);
 			});
 		});
 
-		describe('report by week', function () {
-			before(function (done) {
-				events.report({event: 'application started', report: 'week', date: '2013-09-29'}, function (err, sum) {
-					error = err;
-					summary = sum;
-					done(err);
-				});
-			});
+		it('should have event data', function () {
+			expect(summary.id).to.equal('app-started');
+			expect(summary.event).to.equal('application started');
+		});
 
-			it('should have event data', function () {
-				expect(summary.id).to.equal('app-started');
-				expect(summary.event).to.equal('application started');
-			});
+		it('should have total', function () {
+			expect(summary.total).to.equal(6);
+		});
+	});
 
-			it('should have total', function () {
-				expect(summary.total).to.equal(18);
+	describe('report by week', function () {
+		before(function (done) {
+			request.get({url: url + '/api/reports/week/' + app + '?event=application started&date=2013-09-29', headers: {'x-access-token': token}, json: true}, function (err, res) {
+				error = err;
+				summary = res.body;
+				done(err);
 			});
 		});
 
-		describe('report by month', function () {
-			before(function (done) {
-				events.report({event: 'application started', report: 'month', date: '2013-09-29'}, function (err, sum) {
-					error = err;
-					summary = sum;
-					done(err);
-				});
-			});
+		it('should have event data', function () {
+			expect(summary.id).to.equal('app-started');
+			expect(summary.event).to.equal('application started');
+		});
 
-			it('should have event data', function () {
-				expect(summary.id).to.equal('app-started');
-				expect(summary.event).to.equal('application started');
-			});
+		it('should have total', function () {
+			expect(summary.total).to.equal(18);
+		});
+	});
 
-			it('should have total', function () {
-				expect(summary.total).to.equal(12);
+	describe('report by month', function () {
+		before(function (done) {
+			request.get({url: url + '/api/reports/month/' + app + '?event=application started&date=2013-09-29', headers: {'x-access-token': token}, json: true}, function (err, res) {
+				error = err;
+				summary = res.body;
+				done(err);
 			});
 		});
 
-		describe('report by period', function () {
-			before(function (done) {
-				events.report({event: 'application started', report: 'period', from: '2013-09-29', to: '2013-09-30'}, function (err, sum) {
-					error = err;
-					summary = sum;
-					done(err);
-				});
-			});
+		it('should have event data', function () {
+			expect(summary.id).to.equal('app-started');
+			expect(summary.event).to.equal('application started');
+		});
 
-			it('should have event data', function () {
-				expect(summary.id).to.equal('app-started');
-				expect(summary.event).to.equal('application started');
-			});
+		it('should have total', function () {
+			expect(summary.total).to.equal(12);
+		});
+	});
 
-			it('should have total', function () {
-				expect(summary.total).to.equal(12);
+	describe('report by period', function () {
+		before(function (done) {
+			request.get({url: url + '/api/reports/period/' + app + '?event=application started&from=2013-09-29&to=2013-09-30', headers: {'x-access-token': token}, json: true}, function (err, res) {
+				error = err;
+				summary = res.body;
+				done(err);
 			});
 		});
-		*/
+
+		it('should have event data', function () {
+			expect(summary.id).to.equal('app-started');
+			expect(summary.event).to.equal('application started');
+		});
+
+		it('should have total', function () {
+			expect(summary.total).to.equal(12);
+		});
+	});
 });
