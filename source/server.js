526eb3ddfed3a163d387a6af7b64e15dbab1724e
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-10 16:51:01 +0200

    server output corrected

--- source/server.js
+++ source/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var seismo = require('./seismo')();
 
-http.createServer(seismo).listen(seismo.get('port'), function () {
-	console.log('seismo server started');
+http.createServer(seismo).listen(seismo.get('port'), function (err) {
+	var env = process.env.NODE_ENV || 'development';
+	console.log('seismo server started on port ' + seismo.get('port') + ' env ' + env);
 });
