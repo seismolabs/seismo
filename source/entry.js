e8b60e897c40a7d5e19bb567263cc685eff6f4f9
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-10 12:04:39 +0200

    updated entry callback

--- source/entry.js
+++ source/entry.js
@@ -3,6 +3,12 @@ var http = require('http');
 module.exports = {
 	start: function(config, callback) {
 		var seismo = require('./seismo')(config);
-		http.createServer(seismo).listen(seismo.get('port'), callback);
+		http.createServer(seismo).listen(seismo.get('port'), function (err) {
+			if (err) {
+				return callback(err);
+			}
+
+			return callback(null, seismo);
+		});
 	}
 };
