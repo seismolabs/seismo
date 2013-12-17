22142ed582ab9609a823b02f812fde81128489dd
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-17 21:11:23 +0200

    stupid issue..

--- source/db/index.js
+++ source/db/index.js
@@ -1,10 +1,10 @@
 var mongo = require('mongojs');
 
 module.exports = function (config) {
-	var connection = connection.connection || 'mongodb://localhost:27017/seismodb';
-	var db = mongo.connect(config.connection, ['events']);
+	var connection = config.connection || 'mongodb://localhost:27017/seismodb';
+	var db = mongo.connect(connection, ['users', 'networks', 'items', 'subscribers']);
 	if (!db) {
-		throw new Error('could not connect to ' + config.connection);
+		throw new Error('could not connect to ' + connection);
 	}
 
 	return db;
